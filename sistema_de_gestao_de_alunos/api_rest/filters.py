a7d825b2170878fcf93d3d5af22a74491a5803f6
Emanuel J N <ejn.tech.innovation@gmail.com>
2024-10-12 10:41:50 -0300

    fix: modifica filtros relacionados as classes aluno e professor.

--- sistema_de_gestao_de_alunos/api_rest/filters.py
+++ sistema_de_gestao_de_alunos/api_rest/filters.py
@@ -1,5 +1,6 @@
 from dj_rql.filter_cls import AutoRQLFilterClass
 from .models import Aluno, Professor, Administrador
+
 class AlunoFilterClass(AutoRQLFilterClass):
     MODEL = Aluno
     FILTERS = (
@@ -7,9 +8,25 @@ class AlunoFilterClass(AutoRQLFilterClass):
             'filter': 'full_name',
             'search': True,
         },
+        {
+            'filter': 'data_inscricao',
+            'field_type': 'datetime',  # Defina o tipo de campo como datetime
+        }
     )
+
 class ProfessorFilterClass(AutoRQLFilterClass):
     MODEL = Professor
+
+    FILTERS = (
+        {
+            'filter': 'full_name',
+            'search': True,
+        },
+        {
+            'filter': 'disciplina',
+            'choices': Professor.DisciplinaChoices.choices,  # adicionar as opções
+        }
+    )
 
 class AdministradorFilterClass(AutoRQLFilterClass):
     MODEL = Administrador
